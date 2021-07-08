24ba9697d32ec02d40dc425d10c78c9dbf76e4c2
hurtn <nick.hurt@gmail.com>
2021-07-08 21:36:59 +0100

    Updated info for managed identity usage

--- SetupDDL.sql
+++ SetupDDL.sql
@@ -34,22 +34,26 @@ create table ranger_policies (
 
 create table ranger_policies_staging (
     ID int,
-    ranger_database NVARCHAR(2000),
     Name NVARCHAR(100),
+    RepositoryName NVARCHAR(2000),   
     Resources  NVARCHAR(2000),
     Paths  NVARCHAR(2000),
-    permMapList nvarchar(4000)
+    permMapList nvarchar(4000),
     [Service Type]  NVARCHAR(100),
     Status  NVARCHAR(100),
     checksum NVARCHAR(400),
-    CONSTRAINT "PK_Policies_Staging" PRIMARY KEY CLUSTERED ("ID") )
+    CONSTRAINT "PK_Policies_Staging" PRIMARY KEY CLUSTERED ("ID","RepositoryName")  )
 
 -- Enable CDC at the Database level
 EXEC sys.sp_cdc_enable_db ;
 
--- Enable CDC on the policies table
-EXEC sys.sp_cdc_enable_table
+-- Enable CDC on the policies table. If running as managed identity, you need to impersonate the MI before running this statement. This requires giving the MI db_owner role. eg.ADD 
+--EXEC sp_addrolemember N'db_owner', N'policysyncdemoapp'
+--Then run the following with a priviledged user substituting the username below accordingly
+execute as user = 'yourruntimeusername'
+ EXEC sys.sp_cdc_enable_table
  @source_schema = 'dbo',
  @source_name = 'ranger_policies',
  @role_name = 'null',
  @supports_net_changes = 1;
+revert
